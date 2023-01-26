6eb575f1db0dadf6908cb5e7525ea0e79fe4a8cc
KCLouw <klouw07@gmail.com>
2023-01-26 22:36:25 +0200

    created html and css file

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+
+function colorPicker(color){
+    document.getElementById("red").style.backgroundColor = color;
+    document.getElementById("orange").style.backgroundColor = color;
+    document.getElementById("red").style.backgroundColor = color;
+    document.getElementById("red").style.backgroundColor = color;
+    document.getElementById("red").style.backgroundColor = color;
+    document.getElementById("red").style.backgroundColor = color;
+    document.getElementById("red").style.backgroundColor = color;
+};
