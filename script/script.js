141156eab3b751e5796143a7800095bd9dcae59b
KCLouw <klouw07@gmail.com>
2023-01-27 22:02:04 +0200

    included "this" keyword
    Learnt that I can use the "this" keyword in a function in the event handler in my DOM to assign the DOM element to the this keyword and subsequently pass it as an argument in my created function parameter

--- script/script.js
+++ script/script.js
@@ -1,11 +1,5 @@
-let red =  document.getElementById("red");
 
-function colorPicker(color){
-    document.getElementById("red").style.background = color;
-    document.getElementById("orange").style.background = color;
-    document.getElementById("yellow").style.background = color;
-    document.getElementById("green").style.backgroundColor = color;
-    document.getElementById("blue").style.backgroundColor = color;
-    document.getElementById("purple").style.backgroundColor = color;
-    document.getElementById("violet").style.backgroundColor = color;
+
+function colorPicker(element, color){
+    element.style.backgroundColor = color;
 };
