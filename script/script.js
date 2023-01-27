e9f12c8cfa211997d269c7c7e52ceac9ca2e29de
KCLouw <klouw07@gmail.com>
2023-01-27 21:24:59 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
-
+let red =  document.getElementById("red");
 
 function colorPicker(color){
-    document.getElementById("red").style.backgroundColor = color;
-    document.getElementById("orange").style.backgroundColor = color;
-    document.getElementById("red").style.backgroundColor = color;
-    document.getElementById("red").style.backgroundColor = color;
-    document.getElementById("red").style.backgroundColor = color;
-    document.getElementById("red").style.backgroundColor = color;
-    document.getElementById("red").style.backgroundColor = color;
+    document.getElementById("red").style.background = color;
+    document.getElementById("orange").style.background = color;
+    document.getElementById("yellow").style.background = color;
+    document.getElementById("green").style.backgroundColor = color;
+    document.getElementById("blue").style.backgroundColor = color;
+    document.getElementById("purple").style.backgroundColor = color;
+    document.getElementById("violet").style.backgroundColor = color;
 };
